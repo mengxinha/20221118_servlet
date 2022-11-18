d5f27a4aa6e9be08038454298940260436af9e03
mengxin <mengxinha_2021@qq.com>
2022-11-18 23:07:02 +0800

    23:

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+
+        // 是 Tomcat 这边打印日志
+        System.out.println("hello");
+
+        // 给客户端返回个 hello
+        resp.getWriter().write("hello");
+    }
+}
